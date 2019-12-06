996ebf0acecb00159414660373f0ad85b17c6f87
Jesse Gould <jesse.gould@codeimmersives.com>
2019-12-06 12:24:39 -0600

    adds solutions

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
 const Ray = () => {
+  return {
+    length : 0,
   
+  push: function(value) {
+  this[this.length] = value
+  this.length ++
+  },
+  pop : function(){
+    const previous = this[this.length - 1];
+    delete this[this.length - 1];
+    this.length--;
+    
+    return previous
+
+  },
+
+  shift: function(){
+    const firstItem = this[0];
+      for (let i = 0; i < this.length - 1; i++) {
+        this[i] = this[i + 1]
+      }
+
+      delete this[this.length - 1];
+      this.length--;
+      return firstItem;
+   
+  },
+
+  unshift : function(value){
+    for (let i = this.length; i > 0; i--) {
+      this[i] = this[i - 1]
+    }
+    this[0] = value;
+    this.length++;
+  },
+}
 }
 
 module.exports = {
